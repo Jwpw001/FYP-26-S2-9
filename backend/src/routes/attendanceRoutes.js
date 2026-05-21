8f2c3e2de42c73afe0d568a7540b0829da3bc218
HeinThuraMin <heinthuraminn@gmail.com>
2026-05-21 18:18:43 +0800

    Add attendance routes with RBAC middleware

--- backend/src/routes/attendanceRoutes.js
+++ backend/src/routes/attendanceRoutes.js
@@ -11,28 +11,50 @@ const {
 } = require("../controllers/attendanceController");
 
 const validate = require("../middleware/validateMiddleware");
+const verifyToken = require("../middleware/authMiddleware");
+const allowRoles = require("../middleware/roleMiddleware");
+const ROLES = require("../constants/roles");
 
 const {
     createAttendanceSchema,
     updateAttendanceSchema
 } = require("../validators/attendanceValidator");
 
-router.get("/", getAttendance);
+router.get(
+    "/",
+    verifyToken,
+    allowRoles(ROLES.MANAGER, ROLES.STAFF),
+    getAttendance
+);
 
-router.get("/:id", getAttendanceById);
+router.get(
+    "/:id",
+    verifyToken,
+    allowRoles(ROLES.MANAGER, ROLES.STAFF),
+    getAttendanceById
+);
 
 router.post(
     "/",
+    verifyToken,
+    allowRoles(ROLES.MANAGER, ROLES.STAFF),
     validate(createAttendanceSchema),
     createAttendance
 );
 
 router.patch(
     "/:id",
+    verifyToken,
+    allowRoles(ROLES.MANAGER, ROLES.STAFF),
     validate(updateAttendanceSchema),
     updateAttendance
 );
 
-router.delete("/:id", deleteAttendance);
+router.delete(
+    "/:id",
+    verifyToken,
+    allowRoles(ROLES.MANAGER),
+    deleteAttendance
+);
 
 module.exports = router;
