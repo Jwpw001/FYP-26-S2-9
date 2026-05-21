88c0224520aabb1390696aae906530267ffe4795
HeinThuraMin <heinthuraminn@gmail.com>
2026-05-21 18:31:29 +0800

    Protect report routes with RBAC middleware

--- backend/src/routes/reportRoutes.js
+++ backend/src/routes/reportRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const router = express.Router();
 
 const {
@@ -10,27 +11,50 @@ const {
 } = require("../controllers/reportController");
 
 const validate = require("../middleware/validateMiddleware");
+const verifyToken = require("../middleware/authMiddleware");
+const allowRoles = require("../middleware/roleMiddleware");
+const ROLES = require("../constants/roles");
 
 const {
     createReportSchema,
     updateReportSchema
 } = require("../validators/reportValidator");
 
-router.get("/", getReports);
-router.get("/:id", getReportById);
+router.get(
+    "/",
+    verifyToken,
+    allowRoles(ROLES.MANAGER, ROLES.COORDINATOR),
+    getReports
+);
+
+router.get(
+    "/:id",
+    verifyToken,
+    allowRoles(ROLES.MANAGER, ROLES.COORDINATOR),
+    getReportById
+);
 
 router.post(
     "/",
+    verifyToken,
+    allowRoles(ROLES.MANAGER, ROLES.COORDINATOR),
     validate(createReportSchema),
     createReport
 );
 
 router.patch(
     "/:id",
+    verifyToken,
+    allowRoles(ROLES.MANAGER, ROLES.COORDINATOR),
     validate(updateReportSchema),
     updateReport
 );
 
-router.delete("/:id", deleteReport);
+router.delete(
+    "/:id",
+    verifyToken,
+    allowRoles(ROLES.MANAGER),
+    deleteReport
+);
 
 module.exports = router;
