7107f796414996b7d0b1573fc853d3e94a2c198c
HeinThuraMin <heinthuraminn@gmail.com>
2026-05-21 18:24:09 +0800

    Protect notification routes with RBAC middleware

--- backend/src/routes/notificationRoutes.js
+++ backend/src/routes/notificationRoutes.js
@@ -11,28 +11,66 @@ const {
 } = require("../controllers/notificationController");
 
 const validate = require("../middleware/validateMiddleware");
+const verifyToken = require("../middleware/authMiddleware");
+const allowRoles = require("../middleware/roleMiddleware");
+const ROLES = require("../constants/roles");
 
 const {
     createNotificationSchema,
     updateNotificationSchema
 } = require("../validators/notificationValidator");
 
-router.get("/", getNotifications);
+router.get(
+    "/",
+    verifyToken,
+    allowRoles(
+        ROLES.MANAGER,
+        ROLES.STAFF,
+        ROLES.COORDINATOR
+    ),
+    getNotifications
+);
 
-router.get("/:id", getNotificationById);
+router.get(
+    "/:id",
+    verifyToken,
+    allowRoles(
+        ROLES.MANAGER,
+        ROLES.STAFF,
+        ROLES.COORDINATOR
+    ),
+    getNotificationById
+);
 
 router.post(
     "/",
+    verifyToken,
+    allowRoles(
+        ROLES.MANAGER,
+        ROLES.COORDINATOR
+    ),
     validate(createNotificationSchema),
     createNotification
 );
 
 router.patch(
     "/:id",
+    verifyToken,
+    allowRoles(
+        ROLES.MANAGER,
+        ROLES.COORDINATOR
+    ),
     validate(updateNotificationSchema),
     updateNotification
 );
 
-router.delete("/:id", deleteNotification);
+router.delete(
+    "/:id",
+    verifyToken,
+    allowRoles(
+        ROLES.MANAGER
+    ),
+    deleteNotification
+);
 
 module.exports = router;
