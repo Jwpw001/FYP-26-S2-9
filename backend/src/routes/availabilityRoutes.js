19cc62abf09d57c493f790ad0404a28151274b8e
HeinThuraMin <heinthuraminn@gmail.com>
2026-05-21 18:14:50 +0800

    Protect availability routes with RBAC middleware

--- backend/src/routes/availabilityRoutes.js
+++ backend/src/routes/availabilityRoutes.js
@@ -11,28 +11,50 @@ const {
 } = require("../controllers/availabilityController");
 
 const validate = require("../middleware/validateMiddleware");
+const verifyToken = require("../middleware/authMiddleware");
+const allowRoles = require("../middleware/roleMiddleware");
+const ROLES = require("../constants/roles");
 
 const {
     createAvailabilitySchema,
     updateAvailabilitySchema
 } = require("../validators/availabilityValidator");
 
-router.get("/", getAvailability);
+router.get(
+    "/",
+    verifyToken,
+    allowRoles(ROLES.MANAGER, ROLES.STAFF, ROLES.COORDINATOR),
+    getAvailability
+);
 
-router.get("/:id", getAvailabilityById);
+router.get(
+    "/:id",
+    verifyToken,
+    allowRoles(ROLES.MANAGER, ROLES.STAFF, ROLES.COORDINATOR),
+    getAvailabilityById
+);
 
 router.post(
     "/",
+    verifyToken,
+    allowRoles(ROLES.MANAGER, ROLES.STAFF, ROLES.COORDINATOR),
     validate(createAvailabilitySchema),
     createAvailability
 );
 
 router.patch(
     "/:id",
+    verifyToken,
+    allowRoles(ROLES.MANAGER, ROLES.STAFF, ROLES.COORDINATOR),
     validate(updateAvailabilitySchema),
     updateAvailability
 );
 
-router.delete("/:id", deleteAvailability);
+router.delete(
+    "/:id",
+    verifyToken,
+    allowRoles(ROLES.MANAGER, ROLES.COORDINATOR),
+    deleteAvailability
+);
 
 module.exports = router;
